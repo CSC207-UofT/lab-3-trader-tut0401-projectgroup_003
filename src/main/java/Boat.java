936f427f1aa926b0b3e74d7685afc86405ae2b5e
Kuan-Lin Kuo <luke900228@gmail.com>
2021-09-27 17:49:36 -0400

    Completed lab task for Luke Kuo

---
+++ src/main/java/Boat.java
@@ -0,0 +1,33 @@
+public class Boat implements Drivable, Tradable {
+    private int maxSpeed;
+
+    public Boat(){
+        this.maxSpeed = 150;
+    }
+
+    @Override
+    public void upgradeSpeed() {
+        this.maxSpeed++;
+    }
+
+    @Override
+    public void downgradeSpeed() {
+        this.maxSpeed--;
+    }
+
+    @Override
+    public int getMaxSpeed() {
+        return this.maxSpeed;
+    }
+
+    @Override
+    public int getPrice() {
+        return 10;
+    }
+
+    @Override
+    public String toString(){
+        String id = super.toString();
+        return id + " (Max speed: " + this.maxSpeed +")";
+    }
+}
